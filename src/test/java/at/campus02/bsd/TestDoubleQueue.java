47d3f48fd89f1b098d4229d02fa6320df77f7661
Omar Oraby <52020494@edu.campus02.at>
2022-06-16 17:57:06 +0200

    updated the tests for double queue
    in addition, added javadoc comments to the tests

--- src/test/java/at/campus02/bsd/TestDoubleQueue.java
+++ src/test/java/at/campus02/bsd/TestDoubleQueue.java
@@ -19,52 +19,52 @@ public class TestDoubleQueue {
     }
 
     /**
-     * Adding simpleDrink 5 times - returns true
-     * Adding a 6th simpleDrink - returns false (max = 5)
+     * Adding doubles 5 times - returns true
+     * Adding a 6th double value - returns false (max = 5)
      */
     @Test
     void testOffer(){
         Assertions.assertEquals(true, doubleq.offer(1.0));
         Assertions.assertEquals(true, doubleq.offer(2.0));
-        Assertions.assertEquals(true, doubleq.offer(simpleDrink));
-        Assertions.assertEquals(true, doubleq.offer(simpleDrink));
-        Assertions.assertEquals(true, doubleq.offer(simpleDrink));
-        Assertions.assertEquals(false, doubleq.offer(simpleDrink));
+        Assertions.assertEquals(true, doubleq.offer(3.0));
+        Assertions.assertEquals(true, doubleq.offer(4.0));
+        Assertions.assertEquals(true, doubleq.offer(5.0));
+        Assertions.assertEquals(false, doubleq.offer(6.0));
     }
 
     /**
-     * Adding simpleDrink to the queue
+     * Adding a double value to the queue
      * Removing it with poll
      */
     @Test
     void testPoll(){
-        doubleq.offer(simpleDrink);
-        Assertions.assertEquals(simpleDrink, doubleq.poll());
+        doubleq.offer(1.0);
+        Assertions.assertEquals(1.0, doubleq.poll());
     }
 
     /**
-     * Adding simpleDrink to the queue
+     * Adding a double value to the queue
      * returning it because it's the first element
      */
     @Test
     void testPeek() {
-        doubleq.offer(simpleDrink);
-        Assertions.assertEquals(simpleDrink, doubleq.peek());
+        doubleq.offer(1.0);
+        Assertions.assertEquals(1.0, doubleq.peek());
     }
 
     /**
-     * Adding simpleDrink to the queue
+     * Adding a double value to the queue
      * Removing it with remove
      */
     @Test
     void testRemove() {
-        doubleq.offer(simpleDrink);
-        Assertions.assertEquals(simpleDrink, doubleq.remove());
+        doubleq.offer(1.0);
+        Assertions.assertEquals(1.0, doubleq.remove());
     }
 
     /**
      * Test if exception will be thrown
-     * Getting element from an empty queue
+     * Getting element (a double value) from an empty queue
      */
     @Test
     void testElement() {
