4881746fcb89a9fd1dc67bd3e8f7dca87952aaca
lukasgrafoner <s50706@edu.campus02.at>
2022-06-16 17:36:31 +0200

    Created testcases for StringQueue class

---
+++ src/test/java/at/campus02/bsd/TestStringQueue.java
@@ -0,0 +1,74 @@
+package at.campus02.bsd;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+public class TestStringQueue {
+    private StringQueue stringq;
+
+    /**
+     * Creates a setup, which is needed for the tests.
+     * Create test-data
+     */
+    @BeforeEach
+    public void setup() {
+        stringq = new StringQueue(5);
+    }
+
+    /**
+     * Adding Strings 5 times - returns true
+     * Adding a 6th String - returns false (max = 5)
+     */
+    @Test
+    void testOffer(){
+        Assertions.assertEquals(true, stringq.offer("one"));
+        Assertions.assertEquals(true, stringq.offer("two"));
+        Assertions.assertEquals(true, stringq.offer("three"));
+        Assertions.assertEquals(true, stringq.offer("four"));
+        Assertions.assertEquals(true, stringq.offer("five"));
+        Assertions.assertEquals(false, stringq.offer("too much"));
+    }
+
+    /**
+     * Adding String to the queue
+     * Removing it with poll
+     */
+    @Test
+    void testPoll(){
+        stringq.offer("test");
+        Assertions.assertEquals("test", stringq.poll());
+    }
+
+    /**
+     * Adding String to the queue
+     * returning it because it's the first element
+     */
+    @Test
+    void testPeek() {
+        stringq.offer("test");
+        Assertions.assertEquals("test", stringq.peek());
+    }
+
+    /**
+     * Adding String to the queue
+     * Removing it with remove
+     */
+    @Test
+    void testRemove() {
+        stringq.offer("test");
+        Assertions.assertEquals("test", stringq.remove());
+    }
+
+    /**
+     * Test if exception will be thrown
+     * Getting element from an empty queue
+     */
+    @Test
+    void testElement() {
+        Assertions.assertThrows(NoSuchElementException.class, () -> { stringq.element();});
+    }
+
+}
