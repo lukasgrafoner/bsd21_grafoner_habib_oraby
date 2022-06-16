f4bd94a63858740d2b5c79cae6287c6c7e086ade
Lukas Grafoner <s50706@edu.campus02.at>
2022-06-16 15:29:09 +0200

    created DrinkQueue class

---
+++ src/main/java/at/campus02/bsd/DrinkQueue.java
@@ -0,0 +1,29 @@
+package at.campus02.bsd;
+
+public class DrinkQueue implements IQueue{
+    
+    @Override
+    public boolean offer(String obj) {
+        return false;
+    }
+
+    @Override
+    public String poll() {
+        return null;
+    }
+
+    @Override
+    public String remove() {
+        return null;
+    }
+
+    @Override
+    public String peek() {
+        return null;
+    }
+
+    @Override
+    public String element() {
+        return null;
+    }
+}
